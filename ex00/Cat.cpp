35615c466a475d980526065c3d7e37073fcaac6f
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-19 19:21:04 +0100

    add: enhance documentation for Cat class constructors, assignment operator, and destructor

--- ex00/Cat.cpp
+++ ex00/Cat.cpp
@@ -1,16 +1,28 @@
 #include "Cat.hpp"
 
+/**
+ * @brief Default constructor for Cat. Sets type to "Cat".
+ */
 Cat::Cat()
 {
 	type = "Cat";
 	std::cout << "[Cat] default constructor called\n";
 }
 
+/**
+ * @brief Copy constructor for Cat.
+ * @param other The Cat object to copy from.
+ */
 Cat::Cat(const Cat& other) : Animal(other)
 {
 	std::cout << "[Cat] copy constructor called\n";
 }
 
+/**
+ * @brief Copy assignment operator for Cat.
+ * @param other The Cat object to assign from.
+ * @return Reference to the assigned Cat object.
+ */
 Cat& Cat::operator=(const Cat& other)
 {
 	std::cout << "[Cat] copy assignment operator called\n";
@@ -19,11 +31,17 @@ Cat& Cat::operator=(const Cat& other)
 	return *this;
 }
 
+/**
+ * @brief Destructor for Cat.
+ */
 Cat::~Cat()
 {
 	std::cout << "[Cat] destructor called\n";
 }
 
+/**
+ * @brief Makes a sound. Prints "Meow!".
+ */
 void Cat::makeSound() const
 {
 	std::cout << "Meow!\n";
