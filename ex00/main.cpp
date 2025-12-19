7cd657a6d346c36d4c0d3cf2352f45056316c789
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-19 18:03:31 +0100

    add: implement main function to demonstrate polymorphism and sound behavior of Animal, Dog, Cat, WrongAnimal, and WrongCat classes

---
+++ ex00/main.cpp
@@ -0,0 +1,54 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+	std::cout << "\n==== Subject-like polymorphism test ====\n";
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << "\n";
+	std::cout << i->getType() << "\n";
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+
+	delete i;
+	delete j;
+	delete meta;
+
+    std::cout << "\n==== Animal copy tests ====\n";
+	Dog a;
+	Dog b(a);
+	Dog c;
+	c = a;
+	a.makeSound();
+	b.makeSound();
+	c.makeSound();
+
+    std::cout << "\n==== WrongAnimal test (no virtual makeSound) ====\n";
+	const WrongAnimal* wa = new WrongAnimal();
+	const WrongAnimal* wc = new WrongCat();
+
+	std::cout << wa->getType() << "\n";
+	std::cout << wc->getType() << "\n";
+	wa->makeSound();
+	wc->makeSound(); // <-- it will call WrongAnimal's makeSound, not WrongCat's
+
+	delete wc;
+	delete wa;
+    
+    std::cout << "\n==== Stack objects (destruction order visible) ====\n";
+    Animal x;
+    Cat y;
+    Dog z;
+    x.makeSound();
+    y.makeSound();
+    z.makeSound();
+
+	return 0;
+}
