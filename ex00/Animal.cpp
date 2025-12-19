4b96ba6f47653f88cda4b03e477f74e9bb98fbbb
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-19 16:52:06 +0100

    Implement Animal class with constructors, destructor, and sound method

---
+++ ex00/Animal.cpp
@@ -0,0 +1,34 @@
+#include "Animal.hpp"
+
+Animal::Animal() : type("Animal")
+{
+	std::cout << "[Animal] default constructor called\n";
+}
+
+Animal::Animal(const Animal& other) : type(other.type)
+{
+	std::cout << "[Animal] copy constructor called\n";
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+	std::cout << "[Animal] copy assignment operator called\n";
+	if (this != &other)
+		type = other.type;
+	return *this;
+}
+
+Animal::~Animal()
+{
+	std::cout << "[Animal] destructor called\n";
+}
+
+std::string Animal::getType() const
+{
+	return type;
+}
+
+void Animal::makeSound() const
+{
+	std::cout << "* generic animal sound *\n";
+}
