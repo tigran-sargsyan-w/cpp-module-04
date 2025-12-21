7f41a41ce7078d7dacb3223bfbc612de44289cd5
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-21 14:43:00 +0100

    feat: implement Cure class inheriting from AMateria with necessary methods

---
+++ ex03/Cure.cpp
@@ -0,0 +1,27 @@
+#include "Cure.hpp"
+#include "ICharacter.hpp"
+
+Cure::Cure() : AMateria("cure") {}
+
+Cure::Cure(const Cure & other) : AMateria(other) {}
+
+Cure::~Cure() {}
+
+Cure & Cure::operator=(const Cure & other)
+{
+	if (this != &other)
+	{
+		AMateria::operator=(other);
+	}
+	return *this;
+}
+
+AMateria* Cure::clone() const
+{
+	return new Cure(*this);
+}
+
+void Cure::use(ICharacter& target)
+{
+	std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
+}
