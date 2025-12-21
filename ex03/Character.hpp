ac13f2e7a15081f27142f417e1c3ae8dacb59798
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-21 18:12:27 +0100

    feat: add Character class implementing ICharacter interface with inventory management

---
+++ ex03/Character.hpp
@@ -0,0 +1,31 @@
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+# include "ICharacter.hpp"
+# include "AMateria.hpp"
+
+class Character : public ICharacter
+{
+	private:
+		std::string name;
+		AMateria* inventory[4];
+
+		void initInventory();
+		void clearInventory();
+		void copyFrom(Character const & other);
+
+	public:
+		Character();
+		Character(std::string const & name);
+		Character(const Character & other);
+		virtual ~Character();
+
+		Character & operator=(const Character & other);
+
+		virtual std::string const & getName() const;
+		virtual void equip(AMateria* m);
+		virtual void unequip(int idx);
+		virtual void use(int idx, ICharacter& target);
+};
+
+#endif
